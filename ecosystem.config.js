71172982ef5fc89fcca1b49e0e5855946997d5ed
lucifer1708 <hello@sumitdhiman.in>
2023-12-10 19:08:51 +0530

    added azure

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+     apps: [
+          {
+               name: "dev-prody",
+               script: "./node_modules/next/dist/bin/next",
+               args: "start -p " + 3000,
+               watch: false,
+               autorestart: true,
+          },
+     ],
+};
