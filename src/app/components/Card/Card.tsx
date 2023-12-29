661100917ce0a6799e0c7b9906ac4524a2a279a9
mehulambastha <mehul.213amb@gmail.com>
2023-12-30 01:24:40 +0530

    fixed typo

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import ImageComponent from '../../events/components/imageComponent'
-import OpenDialogButton from '../../events/components/OpenDialogButton'
+// import React from 'react'
+// import ImageComponent from '../../events/components/imageComponent'
+// import OpenDialogButton from '../../events/components/OpenDialogButton'
 
-interface CardProps{
-  card : {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    thumbnail: string;
-    images: Array<string>;
-  }
-}
+// interface CardProps{
+//   card : {
+//     id: number;
+//     title: string;
+//     description: string;
+//     price: number;
+//     discountPercentage: number;
+//     thumbnail: string;
+//     images: Array<string>;
+//   }
+// }
 
 
 
-const Card = ({card}: CardProps) => {
-  return (
-    <div key={card.id} className="card w-72 max-h-96 md:w-80 lg:w-80 shadow-xl card-custom-background">
-    <figure className="relative h-60">
-      <ImageComponent card={card} />
-    </figure>
-    <div className="card-body items-center">
-      <h2 className="card-title">{card.title}</h2>
-      <p>{card.description}</p>
-      <p>{card.price}</p>
-      <OpenDialogButton card={card} setModalOpenToTrue={openModal}/>
-    </div>
-  </div> 
-  )
-}
+// const Card = ({card}: CardProps) => {
+//   return (
+//     <div key={card.id} className="card w-72 max-h-96 md:w-80 lg:w-80 shadow-xl card-custom-background">
+//     <figure className="relative h-60">
+//       <ImageComponent card={card} />
+//     </figure>
+//     <div className="card-body items-center">
+//       <h2 className="card-title">{card.title}</h2>
+//       <p>{card.description}</p>
+//       <p>{card.price}</p>
+//       <OpenDialogButton card={card} setModalOpenToTrue={openModal}/>
+//     </div>
+//   </div> 
+//   )
+// }
 
-export default Card
+// export default Card
