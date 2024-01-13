e9686968dd8141c4a27dc7aa85c8aca41f2fd1ce
mehulambastha <mehul.213amb@gmail.com>
2024-01-13 17:25:41 +0530

    build fix

--- src/app/participate/page.tsx
+++ src/app/participate/page.tsx
@@ -7,14 +7,14 @@ import Footer from '../components/Footer/Footer';
 import './participatePage.css'; 
 const Participate = () => {
   const [wantsLogin, setwantsLogin] = useState(false);
-  const [animationDirection, setAnimationDirection] = useState<'left' | 'right'>('left');
+  // const [animationDirection, setAnimationDirection] = useState<'left' | 'right'>('left');
 
   return (
     <>
       <Navbar isHomePage={false} />
-      <div className={`min-h-screen pt-24 spaceThemeBackground participateContainer `}>
+      <div className={`min-h-screen pt-24 spaceThemeBackground participateContainer`}>
         <div className="card card-custom-background w-11/12 md:w-1/2 lg:w-1/3 shadow-2xl m-auto align-middle">
-          <div className= {`card-body ${animationDirection === 'left' ? 'slide-in-left' : 'slide-in-right'}`}>
+          <div className= {`card-body`}>
             <h2 className="card-title">{wantsLogin ? <span>Login</span> : <span>Register</span>} to continue</h2>
             <div className="form-control w-full max-w-xs">
               {/* Input form */}
@@ -26,7 +26,6 @@ const Participate = () => {
                       className="cursor-pointer hover:underline-1 link-primary link"
                       onClick={() => {
                         setwantsLogin(true);
-                        setAnimationDirection('left');
                       }}
                     >
                       &nbsp;Login &#129042;
@@ -39,7 +38,6 @@ const Participate = () => {
                       className="cursor-pointer hover:underline-1 link-primary link"
                       onClick={() => {
                         setwantsLogin(false);
-                        setAnimationDirection('right');
                       }}
                     >
                       &nbsp;Here &#129042;
