93ead61d3ef302be8ff5d591fe6ebe166fc311e4
lucifer1708 <hello@sumitdhiman.in>
2024-01-22 18:07:00 +0530

    fixed build issue

--- src/app/components/Modal/modal.jsx
+++ src/app/components/Modal/modal.jsx
@@ -5,121 +5,122 @@ import Backdrop from '../Backdrop/backdrop';
 import { useRouter } from 'next/navigation';
 import './modalStyles.css';
 
-const Modal = ({ cardrecieved, handleClose,isRegisteredEvent }) => {
-  useEffect(() => {
-    console.log('cardrecieved', cardrecieved);
-    console.log('isRegisteredEvent', isRegisteredEvent);
+export const Modal = ({ cardrecieved, handleClose, isRegisteredEvent }) => {
+     useEffect(() => {
+          console.log('cardrecieved', cardrecieved);
+          console.log('isRegisteredEvent', isRegisteredEvent);
 
-  }, [cardrecieved]);
+     }, [cardrecieved]);
 
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        handleClose();
-      }
-    };
+     useEffect(() => {
+          const handleKeyDown = (event) => {
+               if (event.key === 'Escape') {
+                    handleClose();
+               }
+          };
 
-    // window.addEventListener('keydown', handleKeyDown);
+          // window.addEventListener('keydown', handleKeyDown);
 
-    // Remove event listener on cleanup
-    // return () =>
-    //  window.removeEventListener('keydown', handleKeyDown);
+          // Remove event listener on cleanup
+          // return () =>
+          //  window.removeEventListener('keydown', handleKeyDown);
 
-  }, [handleClose]);
+     }, [handleClose]);
 
-  const router = useRouter();
+     const router = useRouter();
 
-  const dropIn = {
-    hidden: {
-      y: -50,
-      opacity: 0,
-    },
-    visible: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        type: 'spring',
-        damping: 25,
-        stiffness: 500,
-      },
-    },
-    exit: {
-      y: 50,
-      opacity: 0,
-    },
-  };
+     const dropIn = {
+          hidden: {
+               y: -50,
+               opacity: 0,
+          },
+          visible: {
+               y: 0,
+               opacity: 1,
+               transition: {
+                    duration: 0.5,
+                    type: 'spring',
+                    damping: 25,
+                    stiffness: 500,
+               },
+          },
+          exit: {
+               y: 50,
+               opacity: 0,
+          },
+     };
 
-  const handleRegister = () => {
-    // if (typeof window !== 'undefined') {
-      const eventId = cardrecieved.id;
-      const registrationUrl = `/event-register/${eventId}`;
-      router.push(registrationUrl);
-    }
-  // };
+     const handleRegister = () => {
+          // if (typeof window !== 'undefined') {
+          const eventId = cardrecieved.id;
+          const registrationUrl = `/event-register/${eventId}`;
+          router.push(registrationUrl);
+     }
+     // };
 
-  const handleAbstractLink = () => {
-    // if (typeof window !== 'undefined' && cardrecieved.abstract_link) {
-  //     window.open(cardrecieved.abstract_link, '_blank');
-  //   } else {
-  //     window.open('http://istenith.com/');
-  //   }
-  // };
+     const handleAbstractLink = () => {
+          // if (typeof window !== 'undefined' && cardrecieved.abstract_link) {
+          //     window.open(cardrecieved.abstract_link, '_blank');
+          //   } else {
+          //     window.open('http://istenith.com/');
+          //   }
+          // };
+     };
 
-  return (
-    <Backdrop onClick={handleClose}>
-      <motion.div
-        onClick={(e) => e.stopPropagation()}
-        variants={dropIn}
-        initial='hidden'
-        animate='visible'
-        exit='exit'
-        className='modal-custom flex flex-col md:flex-row border-slate-950'
-      >
-        <button className="close-button" onClick={handleClose}>&times;</button>
-        <div className='image-container' style={{ maxWidth: '500px', flexShrink: 0 }}>
-          {cardrecieved.poster ? (
-            <Image
-              loader={() => cardrecieved.poster}
-              layout='responsive'
-              width={500}
-              height={500}
-              unoptimized={true}
-              src={cardrecieved.poster}
-              alt='thumbnail'
-            />
-          ) : (
-            <Image
-                loader={() => cardrecieved.poster}
-                layout="responsive"
-                width={300}
-                height={100}
-                src=""
-                alt="event-image"
-              />
-            )}
-        </div>
+     return (
+          <Backdrop onClick={handleClose}>
+               <motion.div
+                    onClick={(e) => e.stopPropagation()}
+                    variants={dropIn}
+                    initial='hidden'
+                    animate='visible'
+                    exit='exit'
+                    className='modal-custom flex flex-col md:flex-row border-slate-950'
+               >
+                    <button className="close-button" onClick={handleClose}>&times;</button>
+                    <div className='image-container' style={{ maxWidth: '500px', flexShrink: 0 }}>
+                         {cardrecieved.poster ? (
+                              <Image
+                                   loader={() => cardrecieved.poster}
+                                   layout='responsive'
+                                   width={500}
+                                   height={500}
+                                   unoptimized={true}
+                                   src={cardrecieved.poster}
+                                   alt='thumbnail'
+                              />
+                         ) : (
+                              <Image
+                                   loader={() => cardrecieved.poster}
+                                   layout="responsive"
+                                   width={300}
+                                   height={100}
+                                   src=""
+                                   alt="event-image"
+                              />
+                         )}
+                    </div>
 
-        <div className='flex-grow flex-col gap-4 justify-center items-center'>
-          <div role='tablist' className='tabs w-11/12 mx-auto h-4/5 tabs-lifted'>
+                    <div className='flex-grow flex-col gap-4 justify-center items-center'>
+                         <div role='tablist' className='tabs w-11/12 mx-auto h-4/5 tabs-lifted'>
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Event' defaultChecked />
-            <div role='tabpanel' className='tab-content h-full bg-base-100 border-base-300 rounded-box p-6'>
-              <h2>{cardrecieved.name}</h2>
-            </div>
+                              <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Event' defaultChecked />
+                              <div role='tabpanel' className='tab-content h-full bg-base-100 border-base-300 rounded-box p-6'>
+                                   <h2>{cardrecieved.name}</h2>
+                              </div>
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Description' />
-            <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
-              {cardrecieved.description}
-            </div>
+                              <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Description' />
+                              <div role='tabpanel' className='tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                                   {cardrecieved.description}
+                              </div>
 
-            <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Date' />
-            <div role='tabpanel' className='h-full tab-content bg-base-100 border-base-300 rounded-box p-6'>
-              {cardrecieved.date}
-            </div>
-          </div>
-        <div className='flex gap-3 btns'>
-            {/* <motion.button
+                              <input type='radio' name='my_tabs_2' role='tab' className='tab' aria-label='Date' />
+                              <div role='tabpanel' className='h-full tab-content bg-base-100 border-base-300 rounded-box p-6'>
+                                   {cardrecieved.date}
+                              </div>
+                         </div>
+                         <div className='flex gap-3 btns'>
+                              {/* <motion.button
               className='btn btn-primary max-w-xs'
               onClick={handleRegister}
               whileHover={{ scale: 1.1 }}
@@ -128,42 +129,40 @@ const Modal = ({ cardrecieved, handleClose,isRegisteredEvent }) => {
               Register
             </motion.button> */}
 
-            {isRegisteredEvent ? (
-            <motion.button
-              className='btn btn-disabled max-w-xs'
-              whileHover={{ scale: 1 }}
-              whileTap={{ scale: 0.95 }}
-              disabled
-            >
-              Registered
-            </motion.button>
-          ) : (
-              <motion.button
-                className='btn btn-primary max-w-xs'
-                onClick={handleRegister}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                Register
-              </motion.button>
-              )}
+                              {isRegisteredEvent ? (
+                                   <motion.button
+                                        className='btn btn-disabled max-w-xs'
+                                        whileHover={{ scale: 1 }}
+                                        whileTap={{ scale: 0.95 }}
+                                        disabled
+                                   >
+                                        Registered
+                                   </motion.button>
+                              ) : (
+                                   <motion.button
+                                        className='btn btn-primary max-w-xs'
+                                        onClick={handleRegister}
+                                        whileHover={{ scale: 1.1 }}
+                                        whileTap={{ scale: 0.95 }}
+                                   >
+                                        Register
+                                   </motion.button>
+                              )}
 
-            <motion.button
-              className='btn btn-secondary max-w-xs'
-              onClick={handleAbstractLink}
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              Abstract
-            </motion.button>
-      </div>
-      </div>
-      </motion.div>
-    </Backdrop>
-  );
+                              <motion.button
+                                   className='btn btn-secondary max-w-xs'
+                                   onClick={handleAbstractLink}
+                                   whileHover={{ scale: 1.1 }}
+                                   whileTap={{ scale: 0.95 }}
+                              >
+                                   Abstract
+                              </motion.button>
+                         </div>
+                    </div>
+               </motion.div>
+          </Backdrop>
+     );
 };
-}
 
-export default Modal;
 
 
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,12 @@
 // @ts-ignore
 "use client"
-import React, {use, useEffect, useState} from 'react'
+import React, { use, useEffect, useState } from 'react'
 import ImageComponent from './components/imageComponent';
 import OpenDialogButton from './components/OpenDialogButton';
 import { TypeAnimation } from 'react-type-animation';
 import Navbar from '../components/Navbar/Navbar';
 import { AnimatePresence } from 'framer-motion';
-import Modal from '../components/Modal/Modal';
+import {Modal} from '../components/Modal/modal';
 import Skeleton from '../components/SkeletonCard/Skeleton';
 import Footer from '../components/Footer/Footer';
 import fetchUserData from '../components/fetchUserData';
@@ -14,235 +14,235 @@ import axios from 'axios';
 import Loader from "../LoaderEvent"
 
 
-interface Card{
-  name:string;
-  date_time :string;
-  date :string;
-  id: number;
-  title: string;
-  description: string;
-  poster : string;
-  price: number;
-  discountPercentage: number;
-  thumbnail: string;
-  images: Array<string>;
+interface Card {
+     name: string;
+     date_time: string;
+     date: string;
+     id: number;
+     title: string;
+     description: string;
+     poster: string;
+     price: number;
+     discountPercentage: number;
+     thumbnail: string;
+     images: Array<string>;
 }
 
 interface User {
-  registered_events: {
-    is_live_events: Event[];
-    is_completed_events: Event[];
-    is_upcoming_events: Event[];
-  };
-  username : string;
-  user_id : string;
-  email : string;
+     registered_events: {
+          is_live_events: Event[];
+          is_completed_events: Event[];
+          is_upcoming_events: Event[];
+     };
+     username: string;
+     user_id: string;
+     email: string;
 }
 
 
 interface Event {
-  id: number;
-  date_time: string;
-  date: string;
-  name : string;
-  description : string;
-  poster: string;
-  thumbnail: string;
-  is_live : boolean;
-  is_team_event : boolean;
+     id: number;
+     date_time: string;
+     date: string;
+     name: string;
+     description: string;
+     poster: string;
+     thumbnail: string;
+     is_live: boolean;
+     is_team_event: boolean;
 }
 
 const Page = () => {
-  const [cardData, setCardData] = useState<Card[]>([])
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [registeredEventIds, setRegisteredEventIds] = useState([])
-  const [events, setEvents] = useState<Event[]>([]);
-  const [registeredEvents, setRegisteredEvents] = useState<Event[]>([]);
-  const [nonRegisteredEvents, setNonRegisteredEvents] = useState<Event[]>([]);
-  const [user, setUser] = useState<User | null>(null)
-  
-  const [loading, setLoading] = useState(true);
-  function getMonthName(monthIndex:any) {
-    const months = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-    return months[monthIndex];
-  }
-  
+     const [cardData, setCardData] = useState<Card[]>([])
+     const [isLoaded, setIsLoaded] = useState(false)
+     const [registeredEventIds, setRegisteredEventIds] = useState([])
+     const [events, setEvents] = useState<Event[]>([]);
+     const [registeredEvents, setRegisteredEvents] = useState<Event[]>([]);
+     const [nonRegisteredEvents, setNonRegisteredEvents] = useState<Event[]>([]);
+     const [user, setUser] = useState<User | null>(null)
 
-  useEffect(() => {
-    // Simulate an asynchronous task
-    const fetchData = async () => {
-      // Your asynchronous task goes here
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      setLoading(false);
-    };
+     const [loading, setLoading] = useState(true);
+     function getMonthName(monthIndex: any) {
+          const months = [
+               'January', 'February', 'March', 'April', 'May', 'June',
+               'July', 'August', 'September', 'October', 'November', 'December'
+          ];
+          return months[monthIndex];
+     }
 
-    fetchData();
-  }
-  , []);
-  
-  
-  useEffect(()=>{
-    const fetchData = async () => {
-      try{
-        const res = await fetch('https://api.prody.istenith.com/api/events/')
-        const resJson = await res.json()
-        const formattedData = resJson.map((event:Event) => {
-          const eventDate = new Date(event.date_time);
-          
-          const formattedDate = `${eventDate.getDate()} ${getMonthName(eventDate.getMonth())} ${eventDate.getFullYear()}`;
-          return {
-            ...event,
-            date: formattedDate,
+
+     useEffect(() => {
+          // Simulate an asynchronous task
+          const fetchData = async () => {
+               // Your asynchronous task goes here
+               await new Promise(resolve => setTimeout(resolve, 2000));
+               setLoading(false);
           };
-        });
-        // console.log(formattedData)
-        setCardData(formattedData)
-        setIsLoaded(true)
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
-    fetchData()
-  }, [])
 
-  useEffect(()=>{
-    const fetchUser = async () => {
-      const res = await fetch('https://api.prody.istenith.com/api/events/', { next: { revalidate: 60 } })
-      const events = await res.json()
-      console.log("events in the fetchuse: ", events)
-      const storedToken = localStorage.getItem('myJwtToken');
-      const response = await axios.get('https://api.prody.istenith.com/api/auth/user/', {
-        headers: {
-          Authorization: `${storedToken}`,
-        },
-      });
-
-      console.log("response.data.user2",response.data.user)
-      const user = response.data.user
-      
-      const { is_live_events, is_completed_events, is_upcoming_events } = user.registered_events;
-      const userRegisteredEvents = [...is_live_events, ...is_completed_events, ...is_upcoming_events];
-      const userRegisteredEventsAlternate = events.filter((event: any) => {
-        return userRegisteredEvents.some(registeredEvent => registeredEvent.id === event.id);
-      });
-      console.log("userRegisteredEventsAlternate",userRegisteredEventsAlternate)
-
-      setRegisteredEvents(userRegisteredEventsAlternate);
-  
-      const userNonRegisteredEvents = events.filter((event: any) => {
-        return !userRegisteredEvents.some(registeredEvent => registeredEvent.id === event.id);
-      });
-      setNonRegisteredEvents(userNonRegisteredEvents);
-    }
-    fetchUser()
-  
-  },[])
-
-  useEffect(() => {
-
-    console.log('Registered Events:', registeredEvents);
-    console.log('Non-Registered Events:', nonRegisteredEvents);
-  }, [registeredEvents, nonRegisteredEvents]);
+          fetchData();
+     }
+          , []);
 
 
-  const [modalOpen, setModalOpen] = useState(false)
-  const [selectedCard, setSelectedCard] = useState<Card | null>(null);
+     useEffect(() => {
+          const fetchData = async () => {
+               try {
+                    const res = await fetch('https://api.prody.istenith.com/api/events/')
+                    const resJson = await res.json()
+                    const formattedData = resJson.map((event: Event) => {
+                         const eventDate = new Date(event.date_time);
+
+                         const formattedDate = `${eventDate.getDate()} ${getMonthName(eventDate.getMonth())} ${eventDate.getFullYear()}`;
+                         return {
+                              ...event,
+                              date: formattedDate,
+                         };
+                    });
+                    // console.log(formattedData)
+                    setCardData(formattedData)
+                    setIsLoaded(true)
+               } catch (error) {
+                    console.log('error', error)
+               }
+          }
+          fetchData()
+     }, [])
+
+     useEffect(() => {
+          const fetchUser = async () => {
+               const res = await fetch('https://api.prody.istenith.com/api/events/', { next: { revalidate: 60 } })
+               const events = await res.json()
+               console.log("events in the fetchuse: ", events)
+               const storedToken = localStorage.getItem('myJwtToken');
+               const response = await axios.get('https://api.prody.istenith.com/api/auth/user/', {
+                    headers: {
+                         Authorization: `${storedToken}`,
+                    },
+               });
+
+               console.log("response.data.user2", response.data.user)
+               const user = response.data.user
+
+               const { is_live_events, is_completed_events, is_upcoming_events } = user.registered_events;
+               const userRegisteredEvents = [...is_live_events, ...is_completed_events, ...is_upcoming_events];
+               const userRegisteredEventsAlternate = events.filter((event: any) => {
+                    return userRegisteredEvents.some(registeredEvent => registeredEvent.id === event.id);
+               });
+               console.log("userRegisteredEventsAlternate", userRegisteredEventsAlternate)
+
+               setRegisteredEvents(userRegisteredEventsAlternate);
+
+               const userNonRegisteredEvents = events.filter((event: any) => {
+                    return !userRegisteredEvents.some(registeredEvent => registeredEvent.id === event.id);
+               });
+               setNonRegisteredEvents(userNonRegisteredEvents);
+          }
+          fetchUser()
+
+     }, [])
+
+     useEffect(() => {
+
+          console.log('Registered Events:', registeredEvents);
+          console.log('Non-Registered Events:', nonRegisteredEvents);
+     }, [registeredEvents, nonRegisteredEvents]);
 
 
-  const openModal = (card: Card) => {
-    console.log("open modal clicked")
-    console.log(`Card recieved: ${card}`)
-    console.log(card)
-    setSelectedCard(card);
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-    setSelectedCard(null);
-  };
+     const [modalOpen, setModalOpen] = useState(false)
+     const [selectedCard, setSelectedCard] = useState<Card | null>(null);
 
 
-  return (
-    <div className='h-min'>
-      <div>
-      {loading && (
-        <Loader />
-      )    
-        
-      }
-    </div> 
-      <Navbar isHomePage={false}/>    
-        <div className='mainEventsPage text-center pt-20'>
-          <h1 className='beyonderFont text-5xl events-main-text'>
-            Events
-          </h1>
+     const openModal = (card: Card) => {
+          console.log("open modal clicked")
+          console.log(`Card recieved: ${card}`)
+          console.log(card)
+          setSelectedCard(card);
+          setModalOpen(true);
+     };
 
-            <h2 className='spaceFont hidden lg:block text-l m-4'>
-              &nbsp;
-              <TypeAnimation
-                sequence={['PRODYOGIKI', 500, 'by iste', 500]}
-                repeat={Infinity}
-                cursor={false}
-                speed={{type: 'keyStrokeDelayInMs', value: 125}}
-              />
-              &nbsp;
-            </h2>
+     const closeModal = () => {
+          setModalOpen(false);
+          setSelectedCard(null);
+     };
 
-            {/* <h2 className='spaceFont block lg:hidden text-3xl m-4'>
+
+     return (
+          <div className='h-min'>
+               <div>
+                    {loading && (
+                         <Loader />
+                    )
+
+                    }
+               </div>
+               <Navbar isHomePage={false} />
+               <div className='mainEventsPage text-center pt-20'>
+                    <h1 className='beyonderFont text-5xl events-main-text'>
+                         Events
+                    </h1>
+
+                    <h2 className='spaceFont hidden lg:block text-l m-4'>
+                         &nbsp;
+                         <TypeAnimation
+                              sequence={['PRODYOGIKI', 500, 'by iste', 500]}
+                              repeat={Infinity}
+                              cursor={false}
+                              speed={{ type: 'keyStrokeDelayInMs', value: 125 }}
+                         />
+                         &nbsp;
+                    </h2>
+
+                    {/* <h2 className='spaceFont block lg:hidden text-3xl m-4'>
               &nbsp;
                 Prodyogiki
               &nbsp;
             </h2>             */}
-            <br />
+                    <br />
 
 
-            <div className="custom-flex-container">
-            {isLoaded ? 
-              cardData.map(card => {   
-                // const isEventRegistered = registeredEvents.some(registeredEvent => registeredEvent.id === card.id);
+                    <div className="custom-flex-container">
+                         {isLoaded ?
+                              cardData.map(card => {
+                                   // const isEventRegistered = registeredEvents.some(registeredEvent => registeredEvent.id === card.id);
 
-                return (
-                  <div key={card.id} className="custom-card">
-                    <figure className="custom-card-figure">
-                        <ImageComponent poster={card.poster} />
-                    </figure>
-                    <div className="custom-card-body">
-                      <h2 className="custom-card-title mt-2">{card.name}</h2>
-                      {/* <p>{card.date}</p> */}
-                      <br />
-                      <OpenDialogButton card={card} setModalOpenToTrue={openModal}/>
+                                   return (
+                                        <div key={card.id} className="custom-card">
+                                             <figure className="custom-card-figure">
+                                                  <ImageComponent poster={card.poster} />
+                                             </figure>
+                                             <div className="custom-card-body">
+                                                  <h2 className="custom-card-title mt-2">{card.name}</h2>
+                                                  {/* <p>{card.date}</p> */}
+                                                  <br />
+                                                  <OpenDialogButton card={card} setModalOpenToTrue={openModal} />
+                                             </div>
+                                        </div>
+                                   );
+                              })
+                              :
+                              <Skeleton num={3} />
+                         }
                     </div>
-                  </div>  
-                );
-              })
-              :
-              <Skeleton num={3} />
-            }
+
+
+
+                    <Footer />
+               </div>
+               <AnimatePresence
+                    initial={false}
+                    mode='wait'
+               >
+                    {modalOpen && (
+                         <Modal
+                              cardrecieved={selectedCard}
+                              handleClose={closeModal}
+                              isRegisteredEvent={registeredEvents.some(registeredEvent => registeredEvent.id === selectedCard?.id)}
+                         />
+                    )}
+
+               </AnimatePresence>
           </div>
-
-
-
-        <Footer />
-        </div>
-        <AnimatePresence
-          initial={false}
-          mode='wait'
-        >
-        {/* {modalOpen && (
-            <Modal
-              cardrecieved={selectedCard}
-              handleClose={closeModal}
-              isRegisteredEvent={registeredEvents.some(registeredEvent => registeredEvent.id === selectedCard?.id)}
-            />
-          )} */}
-
-        </AnimatePresence>
-    </div>
-  )
+     )
 }
 
 export default Page
